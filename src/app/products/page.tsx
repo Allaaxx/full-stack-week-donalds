0df3578401d7e3d84753c9d90da9a9c89b4cf232
Allaaxx <allanrao358@gmail.com>
2025-02-19 19:36:28 -0300

    chore: add eslint sort plugin and prettier tailwind plugin

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,9 +3,9 @@ import { Input } from "@/components/ui/input";
 
 const ProductPage = () => {
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-slate-500">
-      <div className=" border-white rounded-xl p-4 bg-white shadow-lg flex justify-center items-center flex-col gap-5">
-        <h1 className="text-4xl font-bold ">Product Page</h1>
+    <div className="flex h-screen w-screen items-center justify-center bg-slate-500">
+      <div className="flex flex-col items-center justify-center gap-5 rounded-xl border-white bg-white p-4 shadow-lg">
+        <h1 className="text-4xl font-bold">Product Page</h1>
         <Input placeholder="Product Name" />
         <Button> Buy</Button>
       </div>
