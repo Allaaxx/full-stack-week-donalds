bab35faaadb350931513a55b5c0b816f0f8e9bba
Allaaxx <allanrao358@gmail.com>
2025-02-19 19:23:31 -0300

    chore: update app fonts

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -5,7 +5,7 @@ const ProductPage = () => {
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-slate-500">
       <div className=" border-white rounded-xl p-4 bg-white shadow-lg flex justify-center items-center flex-col gap-5">
-        <h1 className="text-4xl font-bold text-lime-400">Product Page</h1>
+        <h1 className="text-4xl font-bold ">Product Page</h1>
         <Input placeholder="Product Name" />
         <Button> Buy</Button>
       </div>
