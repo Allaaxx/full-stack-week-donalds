44ed1109158755542c34e96bb3172125185a87ec
Allaaxx <allanrao358@gmail.com>
2025-02-23 17:23:21 -0300

    feat: add decrease product quantity functionality in cart context and component

--- src/app/[slug]/menu/components/cart-product-item.tsx
+++ src/app/[slug]/menu/components/cart-product-item.tsx
@@ -1,16 +1,18 @@
 import { ChevronLeftIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import Image from "next/image";
+import { useContext } from "react";
 
 import { Button } from "@/components/ui/button";
 import { formatCurrency } from "@/helpers/format-currency";
 
-import { CartProduct } from "../contexts/carts";
+import { CartContext, CartProduct } from "../contexts/carts";
 
 interface CartItemProps {
   product: CartProduct;
 }
 
 const CartProductItem = ({ product }: CartItemProps) => {
+  const { decreaseProductQuantity } = useContext(CartContext);
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center gap-3">
@@ -20,13 +22,15 @@ const CartProductItem = ({ product }: CartItemProps) => {
         </div>
 
         <div className="space-y-1">
-          <p className="text-xs max-w-[90%] truncate text-ellipsis">{product.name}</p>
+          <p className="max-w-[90%] truncate text-ellipsis text-xs">
+            {product.name}
+          </p>
           <p className="text-sm font-semibold">
             {formatCurrency(product.price)}
           </p>
           {/* quantidade */}
           <div className="flex items-center gap-1 text-center">
-            <Button className="h-7 w-7 rounded-lg" variant={"outline"}>
+            <Button className="h-7 w-7 rounded-lg" variant={"outline"} onClick={() => decreaseProductQuantity(product.id)}>
               <ChevronLeftIcon />
             </Button>
             <p className="w-7 text-xs">{product.quantity}</p>
